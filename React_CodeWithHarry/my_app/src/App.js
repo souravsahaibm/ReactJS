a91cd652203467869714ed8fd5c1ff1ceb62c1da
Sourav Saha8 <sourav.saha8@ibm.com>
2024-07-21 22:50:08 +0530

    ex of func comp

--- React_CodeWithHarry/my_app/src/App.js
+++ React_CodeWithHarry/my_app/src/App.js
@@ -1,15 +1,30 @@
 
 import './App.css';
+import Accordion from './Components/Accordion';
 import Navbar from './Navbar';
 import TextForm from './TextForm';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-     <Navbar title="TextUtil" about="About Us"/>
-     {/* <Navbar title="TextUtil" /> */}
-     <TextForm/>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Navbar title="TextUtil" about="About Us" />
+          {/* <Navbar title="TextUtil" /> */}
+          <Routes>
+            <Route exact path="/about" Component={Accordion}>
+            </Route>
+            <Route exact path="/" Component={TextForm}>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
